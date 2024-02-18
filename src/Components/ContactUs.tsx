2cee87d711bbee16c5efb644fe346900fb1b2b31
VoYUm <91035200+VoYUm@users.noreply.github.com>
2024-02-18 14:50:58 -0600

    Add files via upload

---
+++ src/Components/ContactUs.tsx
@@ -0,0 +1,51 @@
+import HappyBunchChat from '../Images/Happy Bunch Chat.png';
+import user_4_fill from '../Images/user_4_fill.png';
+import '../Styles/ContactUs.modules.css';
+
+interface Props {
+    aboutUsHandler: () => void;
+    contactHandler: () => void;
+    FAQHandler: () => void;
+}
+
+function ContactUs({aboutUsHandler, contactHandler, FAQHandler}: Props) {
+    return (
+        <div>
+            <div id="buttons">
+                <button className="info-button" onClick={aboutUsHandler}> About Us </button>
+                <button className="info-button" onClick={contactHandler}> Contact </button>
+                <button className="info-button" onClick={FAQHandler}> FAQ </button>
+            </div>
+            <div id="website-title">Website.com</div>
+            <div id="contact-us"> Contact Us </div>
+            <div id="words">
+                LET'S CONNECT! We're here to help and would love to hear from you! Whether you have questions, comments, suggestions, or just want to chat, you can reach out to us via the contact form on this page.
+            </div>
+
+            <div id="talk-to-us">
+                <form>
+                    <div id="name-fields">
+                            
+                        <input type="text" id="firstname" name="firstname" placeholder="First name"/>
+
+                            
+                        <input type="text" id="lastname" name="lastname" placeholder="Last name"/><br />
+
+
+                            
+                        <input type="text" id="email-address" name="email-address" placeholder="Email address"/><br />
+                    </div>
+                
+                    <input type="text" id="content" name="content" placeholder="What do you want to talk to us..." /><br />
+
+                    <button type="submit" id="submit"> Submit </button>
+                    
+                </form>
+            </div>
+            <img id="icon" src={user_4_fill} alt="user icon" />
+            <img id="hbc" src={HappyBunchChat} alt="contact page image" />
+        </div>
+    )
+}
+
+export default ContactUs;
---
+++ src/Components/LogIn.tsx
@@ -0,0 +1,62 @@
+import HumaaansWireframe from '../Images/Humaaans Wireframe.png';
+import '../Styles/login.modules.css';
+
+interface Props {
+    backToHomeHandler: () => void;
+}
+
+function LogIn({backToHomeHandler}: Props) {
+    return (
+        <div style={{
+            margin: 0,
+            padding: 0,
+            height: '100vh', // Set body height to fill the viewport
+            background: 'linear-gradient(to right, #6358DC 50%, white 50%)' // Create a linear gradient background
+          }}>
+            <div className="blue-half">
+                <img id="wireframe" src={HumaaansWireframe} alt="login image" />
+            </div>
+
+            <div className="white-half">
+                <button id='back' onClick={backToHomeHandler}>
+                    &lt; Back
+                </button>
+                <div id="welcome">
+                    Welcome to
+                </div>
+
+                <div id="website-name">
+                    website
+                </div>
+                <div id="bottom-right-container">
+                    <div id="account">
+                        Account Log in
+                    </div>
+
+                    <div id="description">
+                        If you are already a member you can login with your email address and password.
+                    </div>
+
+                    <div id="username-and-password">
+                        <form>
+                            <label htmlFor="username"> User name</label><br />
+                            <input type="text" id="username" name="username" /><br />
+
+                            <label htmlFor="password"> Password</label><br />
+                            <input type="text" id="password" name="password" /><br />
+
+                            <input type="checkbox" id="rememberme" defaultChecked />
+                            Remember me <br />
+                            <button type="submit" id="login"> Log In </button>
+                        </form>
+                    </div>
+                    <div>
+                        Don't have an account? Sign up here
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LogIn;
