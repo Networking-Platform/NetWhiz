7bfe5a082270be2c4708402bbd6d5ea485433cb1
Tyler Liu <cliu232@illinois.edu>
2024-02-21 17:30:34 -0600

    profile and inventory change
    complete most profile starting website
    make an inventory website and moved same sidebar and top buttons to inventory from profile
    Make inventory and profile button for sidebar
    Added profile button on the top

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import Home from './Components/Home';
+import LogIn from './Components/LogIn';
+import ContactUs from './Components/ContactUs';
+import AboutUs from './Components/AboutUs';
+import FAQ from './Components/FAQ';
+import Profile from './Components/Profile';
+import Inventory from './Components/Inventory';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const goToLogin = () => {
+    setCurrentPage('LogIn');
+  }
+
+  const backToHome = () => {
+    setCurrentPage('Home');
+  }
+
+  const toContactPage = () => {
+    setCurrentPage('ContactUs');
+  }
+
+  const toAboutUsPage = () => {
+    setCurrentPage('AboutUs');
+  }
+
+  const toFAQPage = () => {
+    setCurrentPage('FAQ');
+  }
+
+  const toProfilePage = () => {
+    setCurrentPage('Profile');
+  }
+
+  const toInventoryPage = () => {
+    setCurrentPage('Inventory');
+  }
+
+  return (
+    <div>
+      {currentPage === 'Home' ? <Home 
+                                  getStartedHandler={goToLogin} 
+                                  aboutUsHandler={toAboutUsPage} 
+                                  contactHandler={toContactPage} 
+                                  FAQHandler={toFAQPage} 
+                                  ProfileHandler={toProfilePage} 
+                                /> : null}
+
+      {currentPage === 'LogIn' ? <LogIn 
+                                    backToHomeHandler={backToHome} 
+                                  /> : null}
+
+      {currentPage === 'AboutUs' ? <AboutUs 
+                                      aboutUsHandler={toAboutUsPage} 
+                                      contactHandler={toContactPage} 
+                                      FAQHandler={toFAQPage} 
+                                    /> : null}
+      {currentPage === 'ContactUs' ? <ContactUs 
+                                      aboutUsHandler={toAboutUsPage} 
+                                      contactHandler={toContactPage} 
+                                      FAQHandler={toFAQPage}
+                                    /> : null}
+      {currentPage === 'FAQ' ? <FAQ 
+                                  aboutUsHandler={toAboutUsPage} 
+                                  contactHandler={toContactPage} 
+                                  FAQHandler={toFAQPage}
+                                /> : null}
+      {currentPage === 'Profile' ? <Profile 
+                                      ProfileHandler={toProfilePage} 
+                                      aboutUsHandler={toAboutUsPage} 
+                                      contactHandler={toContactPage} 
+                                      FAQHandler={toFAQPage} 
+                                      getStartedHandler={goToLogin}
+                                      InventoryHandler={toInventoryPage}
+                                    /> : null}
+      {currentPage === 'Inventory' ? <Inventory 
+                                      ProfileHandler={toProfilePage} 
+                                      aboutUsHandler={toAboutUsPage} 
+                                      contactHandler={toContactPage} 
+                                      FAQHandler={toFAQPage} 
+                                      getStartedHandler={goToLogin}
+                                    /> : null}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Components/Home.tsx
@@ -0,0 +1,36 @@
+import user_4_fill from '../Images/user_4_fill.png';
+import humaaansGraph from '../Images/Humaaans Graphs.png';
+import '../Styles/Home.modules.css';
+
+interface Props {
+    getStartedHandler: () => void;
+    aboutUsHandler: () => void;
+    contactHandler: () => void;
+    FAQHandler: () => void;
+    ProfileHandler: () => void;
+}
+
+function Home({getStartedHandler, aboutUsHandler, contactHandler, FAQHandler, ProfileHandler}: Props) {
+    return (
+        <div>
+            <div id="website-title">Website.com</div>
+            <h1> Welcome To Our Computer Networking World!</h1>
+            <div id="intro">
+                This is the website that helps you understand everything of computer networking through games
+            </div>
+            <button id="get-started" onClick={getStartedHandler}>
+                Get Started
+            </button>
+            <div id="buttons">
+                <button className="info-button" onClick={aboutUsHandler}> About Us </button>
+                <button className="info-button" onClick={contactHandler}> Contact </button>
+                <button className="info-button" onClick={FAQHandler}> FAQ </button>
+                <button className="info-button" onClick={ProfileHandler}> Profile </button>
+            </div>
+            <img id="icon" src={user_4_fill} alt="user icon" />
+            <img id="cover" src={humaaansGraph} alt="front page image" />
+        </div>
+    )
+}
+
+export default Home;
